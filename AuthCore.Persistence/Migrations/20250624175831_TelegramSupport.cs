589ead45fcee7878d3171fce1fb4c731401d5eec
Alex Petrachuk <a.petrachuk@gmail.com>
2025-06-24 20:59:27 +0300

    chore: update database migrations

---
+++ AuthCore.Persistence/Migrations/20250624175831_TelegramSupport.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthCore.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class TelegramSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "TelegramId",
+                table: "AspNetUsers",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WhatsAppId",
+                table: "AspNetUsers",
+                type: "character varying(25)",
+                maxLength: 25,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TelegramId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WhatsAppId",
+                table: "AspNetUsers");
+        }
+    }
+}
